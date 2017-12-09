0c61a731a7aea6339b25ad5d95074a9fbbe61107
Lady Jeraldynne Pinzon Capera <lj.pinzon12@uniandes.edu.co>
2017-12-09 16:53:39 -0500

    hi

--- imports/ui/Characters.jsx
+++ imports/ui/Characters.jsx
@@ -5,7 +5,7 @@ class Characters extends Component {
     render() {
         return (
             <div className="characters">
-                <div className="item1"><img src="http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b/standard_xlarge.jpg"/></div>
+                <div className="item1"><img src="http://marvel.dl.llnw.net/wdig/marvel/u/prod/marvel/i/mg/9/20/53176ebd40ad7/portrait_xlarge.jpg"/></div>
                 <div className="item2"><img src="./img/amarillo.png"/></div>
                 <div className="item3"><img src="./img/azul.png"/></div>
                 <div className="item4"><img src="./img/morado.png"/></div>
